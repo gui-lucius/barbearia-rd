8a30a6ae7069bbfe7fe1bca90bfe7fd16b5b8a80
Guilherme Lucius Santos Schutz <guisantosschutz2@gmail.com>
2025-02-04 11:03:50 -0300

    Correção do link para calendario.html

--- agendamentos/urls.py
+++ agendamentos/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import criar_agendamento, horarios_ocupados, home
+from django.shortcuts import render
+
+def calendario (request):
+    return render(request, 'calendario.html')
 
 urlpatterns = [
     path('', home, name='home'),  # Página inicial (renderiza index.html)
     path('api/agendamentos/', criar_agendamento, name='criar_agendamento'),  # Rota para criar agendamento
     path('api/horarios/', horarios_ocupados, name='horarios_ocupados'),  # Rota para ver horários ocupados
+    path('calendario/', calendario, name='calendario'),
+
 ]
+
