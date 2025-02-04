e4aa42db39f8c921583f85fe9fdbc0154f3d5aa8
Guilherme Lucius Santos Schutz <guisantosschutz2@gmail.com>
2025-02-04 08:29:55 -0300

    Correções no settings.py para Heroku

--- barbearia/settings.py
+++ barbearia/settings.py
@@ -3,20 +3,25 @@ from datetime import timedelta
 import os
 import dj_database_url
 
+# Caminho base do projeto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Configuração da chave secreta
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'chave-de-desenvolvimento')
 
-DEBUG = False
+# Modo de depuração
+DEBUG = os.getenv('DEBUG', 'False') == 'True'  # Agora pode ser controlado via Heroku
 
+# Hosts permitidos
 ALLOWED_HOSTS = [
     '127.0.0.1', 
     'localhost', 
     'sua-barbearia.com', 
     'www.sua-barbearia.com',
-    'barbearia-rd-a3b518df45e1.herokuapp.com'  # <- Adicione isso!
+    'barbearia-rd-a3b518df45e1.herokuapp.com'  
 ]
 
+# Aplicações instaladas
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -26,12 +31,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'agendamentos',
     'corsheaders',
-    'sslserver',
 ]
 
+# Middlewares
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Gerenciador de arquivos estáticos no Heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -41,12 +46,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Configuração de URLs
 ROOT_URLCONF = 'barbearia.urls'
 
+# Configuração de templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -59,13 +66,19 @@ TEMPLATES = [
     },
 ]
 
+# Configuração do WSGI
 WSGI_APPLICATION = 'barbearia.wsgi.application'
 
-
+# Configuração do banco de dados (PostgreSQL no Heroku, SQLite localmente)
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}", 
+        conn_max_age=600, 
+        ssl_require=True
+    )
 }
 
+# Validação de senhas
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -73,28 +86,33 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-LANGUAGE_CODE = 'en-us'
+# Configuração de idioma e fuso horário
+LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
+# Configuração de arquivos estáticos
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# Configuração do CORS (evita bloqueio de requisições entre frontend e backend)
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "https://sua-barbearia.com",
 ]
 
+# Configuração de envio de e-mails (agora segura)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'denisbarbeariard@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'senha-padrao')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_FAIL_SILENTLY = True  # Evita que erro no e-mail quebre o site
 
+# Configuração de autenticação JWT
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -108,14 +126,10 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-SECURE_SSL_REDIRECT = True
+# Segurança extra para produção (SSL)
+SECURE_SSL_REDIRECT = not DEBUG  # Só força HTTPS se estiver em produção
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
-
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Pasta onde os arquivos estáticos serão coletados
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # WhiteNoise cuidará dos arquivos estáticos
