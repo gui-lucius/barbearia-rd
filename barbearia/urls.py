d31772649b9a8fea4339ea164ac12bf4ec448b71
Guilherme Lucius Santos Schutz <guisantosschutz2@gmail.com>
2025-02-08 10:14:45 -0300

    Adicionando verificação do Google

--- barbearia/urls.py
+++ barbearia/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.http import JsonResponse
+from django.views.generic import TemplateView
 
 # Teste rápido para ver se o servidor está rodando
 def health_check(request):
@@ -13,4 +14,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('health/', health_check, name='health_check'),
+    path("google5e41cd1aba47309f.html", TemplateView.as_view(template_name="google5e41cd1aba47309f.html")),
 ]
