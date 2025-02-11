1e5f81fb2d6e0484678b1a44edb50bcb51cf46c6
Guilherme Lucius Santos Schutz <guisantosschutz2@gmail.com>
2025-02-11 11:03:20 -0300

    Corrigindo admin.py removendo formatação de data_horario

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,27 +1,18 @@
 from django.contrib import admin
-from .models import Agendamento
-from .models import HorarioBloqueado
-
+from .models import Agendamento, HorarioBloqueado
 
 @admin.register(HorarioBloqueado)
 class HorarioBloqueadoAdmin(admin.ModelAdmin):
     list_display = ('data_horario', 'motivo') 
     search_fields = ('data_horario', 'motivo')
 
-    # 🔥 Força o formato correto no formulário
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == "data_horario":
-            kwargs["widget"].format = '%Y-%m-%d %H:%M:%S'  # Define formato correto
-        return super().formfield_for_dbfield(db_field, **kwargs)
-
-
 @admin.register(Agendamento)
 class AgendamentoAdmin(admin.ModelAdmin):
-    list_display = ('nome_cliente', 'data_horario_reserva', 'status', 'disponivel')  # ✅ Adiciona o campo "disponivel"
-    list_filter = ('status', 'data_horario_reserva', 'disponivel')  # ✅ Filtro para horários disponíveis
+    list_display = ('nome_cliente', 'data_horario_reserva', 'status', 'disponivel')  
+    list_filter = ('status', 'data_horario_reserva', 'disponivel')  
     search_fields = ('nome_cliente', 'email_cliente')
 
-    actions = ["trancar_horarios", "destrancar_horarios"]  # ✅ Ações rápidas no Django Admin
+    actions = ["trancar_horarios", "destrancar_horarios"]
 
     @admin.action(description="Trancar horários selecionados")
     def trancar_horarios(self, request, queryset):
